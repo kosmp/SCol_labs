0b78969a7c5fcadb6f8bda24d5c1e98943aabd41
kosmp <kosmin.2003@mail.ru>
2023-02-24 21:00:55 +0300

    added Task2

---
+++ Lab1/Task2/main.py
@@ -0,0 +1,17 @@
+import sys
+import utilities
+
+
+try:
+    num1=int(input("Enter the first integer number: "))
+    num2=int(input("Enter the second integer number: "))
+    operation=input("Enter add or sub or mul or div: ")
+except ValueError:
+    print("Incorrect number input")
+    sys.exit()
+
+try:
+    result=utilities.calculate(num1, num2, operation)
+    print(result)
+except ZeroDivisionError:
+    print("Error, division by zero.")
